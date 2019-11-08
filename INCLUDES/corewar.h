f112ab1e5ae569046ee41c1afb3d21fdb4ec3f81
Kiana Mills <kmills@mi-f6.21-school.ru>
2019-11-08 14:45:39 +0300

    First commit

---
+++ INCLUDES/corewar.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   filler.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmills <kmills@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/19 19:20:50 by kmills            #+#    #+#             */
+/*   Updated: 2019/09/19 19:20:50 by kmills           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COREWAR_H
+# define COREWAR_H
+# include "../libft/libft.h"
+# include <sys/stat.h>
+# include <sys/xattr.h>
+# include <time.h>
+# include <dirent.h>
+# include <sys/types.h>
+# include <pwd.h>
+# include <uuid/uuid.h>
+# include <grp.h>
+# include <uuid/uuid.h>
+
+#endif
---
+++ SRCS/main.c
@@ -0,0 +1,7 @@
+#include "corewar.h"
+
+int		main(void)
+{
+	ft_printf("HELLO\n");
+	return (0);
+}
